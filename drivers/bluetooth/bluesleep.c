MotorolaMobilityLLC/kernel-msm

bluetooth: Reduce bluesleep wakelock timeout
The bluesleep wakelock timeout can lead to cases where a device which
could have been put into suspend does not go into suspend, since some
bluetooth traffic keeps disrupting the attempt to go to sleep.

Therefore we reduce the bluesleep wakelock timeout period by a factor of
4, so that the chip has a greater probability of entering into suspend
successfully.

Bug: 14900846
Change-Id: I21aad724b3489ccdbfdd862c5049c973fb358175
Signed-off-by: Prashant Malani <pmalani@google.com>

drivers/bluetooth/bluesleep.c
@@ -226,9 +226,9 @@ static void bluesleep_sleep_work(struct work_struct *work)
 			hsuart_power(0);
 
 			/* UART clk is not turned off immediately. Release
-			 * wakelock after 500 ms.
+			 * wakelock after 125 ms.
 			 */
-			wake_lock_timeout(&bsi->wake_lock, HZ / 2);
+			wake_lock_timeout(&bsi->wake_lock, HZ / 8);
 		} else {
 			mod_timer(&tx_timer, jiffies + (TX_TIMER_INTERVAL * HZ));
 			return;
@@ -431,7 +431,7 @@ static void bluesleep_stop(void)
 	if (disable_irq_wake(bsi->host_wake_irq))
 		BT_ERR("Couldn't disable hostwake IRQ wakeup mode");
 
-	wake_lock_timeout(&bsi->wake_lock, HZ / 2);
+	wake_lock_timeout(&bsi->wake_lock, HZ / 8);
 }
 
 static int bluesleep_write_proc_btwrite(struct file *file,

LuizPauloDamaceno/android_kernel_sony_msm8956

Signed-off-by: Chippa-a <vusal1372@gmail.com>

drivers/bluetooth/bluesleep.c
@@ -225,9 +225,9 @@ static void bluesleep_sleep_work(struct work_struct *work)
 			/*Deactivating UART */
 			hsuart_power(HS_UART_OFF);
 			/* UART clk is not turned off immediately. Release
-			 * wakelock after 500 ms.
+			 * wakelock after 125 ms.
 			 */
-			__pm_wakeup_event(&bsi->wake_lock, HZ / 2);
+			__pm_wakeup_event(&bsi->wake_lock, HZ / 8);
 		} else {
 			pr_err("This should never happen.\n");
 			return;
@@ -419,7 +419,7 @@ static void bluesleep_stop(void)
 	atomic_inc(&open_count);
 
 	enable_wakeup_irq(0);
-	__pm_wakeup_event(&bsi->wake_lock, HZ / 2);
+	__pm_wakeup_event(&bsi->wake_lock, HZ / 8);
 }
 
 void bluesleep_setup_uart_port(struct platform_device *uart_dev)