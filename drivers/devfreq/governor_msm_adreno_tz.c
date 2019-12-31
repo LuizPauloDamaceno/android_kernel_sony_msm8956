7a4c3467620172e882ca0138f826ecfa827735c9
LuizPauloDamaceno <luizpaulodamaceno@live.com>
2019-12-30 22:07:04 -0300

    fix compile

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -24,8 +24,6 @@
 #include <soc/qcom/scm.h>
 #include "governor.h"
 
-static bool power_suspended;
-
 static DEFINE_SPINLOCK(tz_lock);
 static DEFINE_SPINLOCK(sample_lock);
 static DEFINE_SPINLOCK(suspend_lock);
